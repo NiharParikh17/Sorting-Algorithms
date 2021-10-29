1529e8b65059222bf2248cecce6bafffc59a7d7a
Nihar Parikh <nihar17999@gmail.com>
2021-10-28 22:23:22 -0400

    Created test for insertion sort algo

---
+++ src/test/java/SortingAlgorithms/SortingAlgorithmsTest.java
@@ -0,0 +1,22 @@
+package SortingAlgorithms;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SortingAlgorithmsTest {
+    private int[] actualArray;
+    private final int[] expectedArray = {14, 27, 31, 36, 40};
+
+    @BeforeEach
+    public void beforeEachAlgorithm() {
+        actualArray = new int[]{36, 14, 27, 40, 31};
+    }
+
+    @Test
+    public void insertionSortTest() {
+        SortingAlgorithms.insertionSort(actualArray);
+        assertArrayEquals(expectedArray, actualArray);
+    }
+}
