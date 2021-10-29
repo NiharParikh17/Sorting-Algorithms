c87e0b804ab03c284da5f8ee8faecb673b058b4e
Nihar Parikh <nihar17999@gmail.com>
2021-10-28 23:35:23 -0400

    Creating counting sort algorithm

--- src/main/java/SortingAlgorithms/SortingAlgorithms.java
+++ src/main/java/SortingAlgorithms/SortingAlgorithms.java
@@ -18,4 +18,44 @@ public class SortingAlgorithms {
             array[j+1] = value;
         }
     }
+
+    /**
+     * Counting Sort algorithm
+     * Worst-case: O(n+m)
+     * where n is length of the array and m is the biggest value in the array
+     * @param array Array to be sorted
+     */
+    public static void countingSort(int[] array) {
+        int[] sortedArray = new int[array.length];
+        int[] count = new int[getMax(array)];
+        // Setting the initial counts to 0
+        for (int i=0; i<count.length; i++)
+            count[i] = 0;
+        // Updating counters for each value
+        for (int i=0; i<array.length; i++)
+            count[array[i]]++;
+        // Updating the counts with array positions
+        for (int i=1; i<count.length; i++)
+            count[i] += count[i-1];
+        // Building the sorted array
+        for (int i=0; i< array.length; i++) {
+            sortedArray[count[array[i]]] = array[i];
+            count[array[i]]--;
+        }
+        // Copying the sorted array to initial array
+        for (int i=0; i<array.length; i++)
+            array[i] = sortedArray[i];
+    }
+
+    /* ******************************
+     # Helper Methods
+     * ******************************/
+
+    private static int getMax(int[] array) {
+        int max = array[0];
+        for (int i=1; i<array.length; i++)
+            if (array[i] > max)
+                max = array[i];
+        return max;
+    }
 }
