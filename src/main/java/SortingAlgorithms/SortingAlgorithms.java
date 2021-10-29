ed1fe9e76933e13c5f6f8ca19c8b5c5bccb2a3ae
Nihar Parikh <nihar17999@gmail.com>
2021-10-28 22:15:39 -0400

    Created the insertion sorting algorithm

--- src/main/java/SortingAlgorithms/SortingAlgorithms.java
+++ src/main/java/SortingAlgorithms/SortingAlgorithms.java
@@ -1,4 +1,21 @@
 package SortingAlgorithms;
 
 public class SortingAlgorithms {
+    /**
+     * Insertion Sort algorithm
+     * Worst-case: O(n^2)
+     * @param array Array to be sorted
+     */
+    public static void insertionSort(int[] array) {
+        int length = array.length;
+        for (int i=1; i<length; i++) {
+            int value = array[i];
+            int j = i-1;
+            while ((j >= 0) && (array[j] > value)) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = value;
+        }
+    }
 }
